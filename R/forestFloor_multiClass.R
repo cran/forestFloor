ae3af99cbd649c7e0a6f9fa87b29b8534c4ae367
Soeren Havelund Welling <SOWE@DTU.DK>
2015-08-03 00:21:49 +0000

    version 1.8.3

---
+++ R/forestFloor_multiClass.R
@@ -0,0 +1,142 @@
+#test multiClass.cpp
+forestFloor_randomForest_multiClass <- function(rf.fit,
+                                     X,
+                                     calc_np = FALSE,
+                                     binary_reg = FALSE,
+                                     ...) { 
+
+  #check the rf.fitbject have a inbag
+  if(is.null(rf.fit$inbag)) stop("input randomForest-object have no inbag, set keep.inbag=T,
+                              try, randomForest(X,Y,keep.inbag=T) for without replacement
+                              and, cinbag(X,Y,keep.inbag=T,keep.forest=T) with replacement
+                              ..cinbag is from trimTrees package...
+                              error condition: if(is.null(rf.fit$inbag))")
+  
+  #make node status a integer matrix
+  ns = rf.fit$forest$nodestatus
+  storage.mode(ns) = "integer"
+  
+  
+  #translate binary classification RF-object, to regression mode
+  if(rf.fit$type=="classification") {
+    #     if(length(levels(rf.fit$y))!=2) stop("no multiclass, must be binary classification.
+    #                                       error condition: if(length(levels(rf.fit$y))!=2")
+    
+    rf.fit$forest$leftDaughter  = rf.fit$forest$treemap[,1,] #translate daughter representation to regression mode
+    rf.fit$forest$rightDaughter = rf.fit$forest$treemap[,2,] 
+    ns[ns==1] = -3  ##translate nodestatus representation to regression mode
+    
+    if(!is.null(rf.fit$inbagCount)) {
+      inbag = rf.fit$inbagCount
+    } else {
+      if(!is.null(rf.fit$inbag)) {
+        inbag = rf.fit$inbag
+      } else {
+        stop("error rf.fit$inbag or rf.fit$inbagCount is missing, 
+             retrain forest with keep.inbag=TRUE")
+      }
+      }
+    } else {
+      stop("this function should only be run for classification")
+  }
+  
+  
+  #preparing data, indice-correction could be moved to C++
+  #a - This should be fethed from RF-object, flat interface
+  ld = rf.fit$forest$leftDaughter-1 #indice correction, first element is 0 in C++ and 1 in R.
+  storage.mode(ld) = "integer"
+  rd = rf.fit$forest$rightDaughter-1
+  storage.mode(rd) = "integer"
+  bv = rf.fit$forest$bestvar-1
+  storage.mode(bv) = "integer"
+  np = rf.fit$forest$nodepred
+  storage.mode(np) = "double"
+  bs = rf.fit$forest$xbestsplit
+  storage.mode(bs) = "double"
+  ib = inbag
+  storage.mode(ib) = "integer"
+  Yd = as.numeric(rf.fit$y)-1
+  storage.mode(Yd) = "integer"
+  ot  = rf.fit$oob.times
+  storage.mode(ot) = "integer"
+  
+  
+  ##recording types of variables
+  xlevels = unlist(lapply(rf.fit$forest$xlevels,length),use.names=F)
+  xl = xlevels
+  storage.mode(xl) = "integer"
+  varsToBeConverted = xlevels>1
+  
+  ##Converting X to Xd, all factors change to level numbers
+  Xd=X
+  for(i in 1:dim(Xd)[2]) {
+    if(varsToBeConverted[i]) {
+      Xd[,i] = as.numeric(Xd[,i])-1  
+    }
+  }  
+  Xd=as.matrix(Xd)
+  storage.mode(Xd) = "double"
+  
+  nClasses = as.integer(max(Yd))+1
+  obs = length(Yd)
+  vars=dim(X)[2]
+  
+  #outout variable
+  localIncrements = rep(0.0,nClasses * obs * vars)
+  storage.mode(localIncrements) = "double"
+  
+  
+  # C++ function, recursively finding increments of all nodes of all trees
+  # where OOB samples are present. vars, obs and ntree is "passed by number"
+  # Anything else is passed by reference. Found increments are imediately
+  # summed to localIncrements matrix.
+  multiTree(
+    #passed by number
+    vars=vars, 
+    obs=obs,             
+    ntree=rf.fit$ntree,
+    nClasses = nClasses,# changed from calculate node pred
+    #passed by reference
+    X=Xd,  #training data, double matrix [obs,vars] 
+    Y=Yd,
+    leftDaughter = ld,  #row indices of left subnodes, integer matrix [nrnodes,ntree] 
+    rightDaughter = rd, #...
+    nodestatus = ns,    #weather node is terminal or not,      
+    xbestsplit = bs,          
+    nodepred = np,          
+    bestvar = bv,
+    inbag = ib,
+    varLevels = xl,
+    ot,  #oob.times
+    localIncrements = localIncrements #output is written directly to localIncrements from C++
+  )
+  
+
+  #restructure to feature contributions obs X var X class
+  localIncrements = unlist(lapply(c(1:(nClasses-1),0),function(i) {
+    localIncrements[(1:length(localIncrements))%%(nClasses)==i]
+  }))
+  
+  
+  #writing out list
+  imp = as.matrix(rf.fit$importance)[,1]
+  out = list(X=X,Y=rf.fit$y,
+             importance = imp,
+             imp_ind = sort(imp,decreasing=TRUE,index.return=TRUE)$ix,
+             FCarray = array(localIncrements,dim=c(obs,vars,nClasses)),
+             sumOfInbags = apply(rf.fit$inbag,1,sum)
+             #  all = mget(ls()) #export everything in list
+  )
+
+  class(out) = "forestFloor_multiClass"
+  return(out)
+}
+
+
+print.forestFloor_multiClass = function(x,...) {
+  cat("this is a forestFloor_multiClass object \n
+      this object can be plotted in 2D with plot(x), see help(plot.forestFloor) \n
+      this object can be plotted in 3D with show3d(x), see help(show3d) \n
+      \n
+      x contains following internal elements: \n ",with(x,ls()))
+}
---
+++ tests/testMultiClass.cpp.R
@@ -0,0 +1,81 @@
+library(randomForest)
+library(forestFloor)
+require(utils)
+
+data(iris)
+iris
+X = iris[,!names(iris) %in% "Species"]
+Y = iris[,"Species"]
+as.numeric(Y)
+rf.test42 = randomForest(X,Y,keep.forest=T,replace=F,keep.inbag=T,samp=15,ntree=100)
+ff.test42 = forestFloor(rf.test42,X,F,F)
+pred = sapply(1:3,function(i) apply(ff.test42$FCarray[,,i],1,sum))+1/3
+rfPred = predict(rf.test42,type="vote",norm.votes=T)
+rfPred[is.nan(rfPred)] = 1/3
+if(cor(as.vector(rfPred),as.vector(pred))^2<0.99) stop("fail testMultiClass")
+attributes(ff.test42)
+args(forestFloor:::plot.forestFloor_multiClass)
+plot(ff.test42,compute_GOF=F,plot_GOF=T,cex=.7,
+     colLists=list(c("#FF0000A5"),
+                   c("#00FF0050"),
+                   c("#0000FF35")))
+
+show3d(ff.test42,1:2,3:4,compute_GOF=T)
+#plot all effect 2D only
+pars = plot_simplex3(ff.test42,Xi=c(1:3),restore_par=F,zoom.fit=NULL,var.col=NULL,fig.cols=2,fig.rows=1,
+               fig3d=F,includeTotal=T,auto.alpha=.4,set_pars=T)
+pars = plot_simplex3(ff.test42,Xi=0,restore_par=F,zoom.fit=NULL,var.col=alist(alpha=.3,cols=1:4),
+               fig3d=F,includeTotal=T,auto.alpha=.8,set_pars=F)
+
+
+
+for (I in ff.test42$imp_ind[1:4])  {
+  #plotting partial OOB-CV separation(including interactions effects), coloured by true class
+  pars = plot_simplex3(ff.test42,Xi=I,restore_par=F,zoom.fit=NULL,var.col=NULL,fig.cols=4,fig.rows=2,
+                 fig3d=F,includeTotal=F,label.col=1:3,auto.alpha=.3,set_pars = (I==ff.test42$imp_ind[1]))
+  #plotting partial OOB-CV separation(including interactinos effects), coloured by varaible value
+  pars = plot_simplex3(ff.test42,Xi=I,restore_par=F,zoom.fit=T,var.col=alist(order=F,alpha=.8),
+                 fig3d=F,includeTotal=(I==4),auto.alpha=.3,set_pars=F)
+}
+
+
+
+
+##more plots
+rm(list=ls())
+library(randomForest)
+library(forestFloor)
+#simulate data
+obs=2500
+vars = 6
+X = data.frame(replicate(vars,rnorm(obs)))
+Y = with(X, X1^2 + sin(X2*pi) + 2 * X3 * X4 + 1 * rnorm(obs))
+Y.class <- Y
+Y.class[Y<=-1] <- 1
+Y.class[Y>1] <- 2
+Y.class[Y.class<1] <- 3
+Y.class <- factor(Y.class)
+#grow a forest, remember to include inbag
+
+rfo=randomForest(X,Y.class,keep.inbag = TRUE,sampsize=1500,ntree=500)
+
+#compute topology
+ff = forestFloor(rfo,X)
+
+#plot
+Col = fcol(ff,1,alpha=.25,orderByImportance=FALSE) #farve bruges her bedst til kende forskel på klasser
+
+plot(ff,
+     plot_GOF=T,
+     colLists=list("#00000010",
+                   "#FF000010",
+                   "#00FF0010"),
+     GOF_col=c("#3A3A3A80",
+               "#FF3A3A80",
+               "#3AFF3A80"))
+
+plot(ff,plot_seq=1:6,
+     colLists=list("#00000002", #farve vector til class1 (length 1 eller 2500)
+                   "#00000002", #farve vector til class2 (length 1 eller 2500)
+                   Col) #tredje  farves med fcol (length 1 eller 2500)
+     ,plot_GOF=T)
